18a92a442693e747698ff842b79b85ff6f4c582c
RahulKabothula <rahul.kabothula@clairvoyant.com>
2021-12-22 12:55:09 +0530

    Read.java changed to Extract.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,21 +6,20 @@ import org.apache.spark.sql.Row;
 public class App {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
 
         //Create SparkSession
         SparkSession sparkSession = Util.getSparkSession();
 
-        //read data
-        Read read = new Read();
+        //Extract data
+        Extract extract = new Extract();
 
         String dept_path = "C:\\Users\\Rahul Kabothula\\Intellij Maven Projects\\retail_db\\src\\main\\resources\\retail_db\\departments\\part-00000";
         String cat_path = "C:\\Users\\Rahul Kabothula\\Intellij Maven Projects\\retail_db\\src\\main\\resources\\retail_db\\categories\\part-00000";
         String prod_path = "C:\\Users\\Rahul Kabothula\\Intellij Maven Projects\\retail_db\\src\\main\\resources\\retail_db\\products\\part-00000";
 
-        Dataset<Row> dept_df = read.readData(sparkSession,"csv", dept_path);
-        Dataset<Row> cat_df = read.readData(sparkSession,"csv",cat_path);
-        Dataset<Row> prod_df = read.readData(sparkSession,"csv",prod_path);
+        Dataset<Row> dept_df = extract.readData(sparkSession,"csv", dept_path);
+        Dataset<Row> cat_df = extract.readData(sparkSession,"csv",cat_path);
+        Dataset<Row> prod_df = extract.readData(sparkSession,"csv",prod_path);
 
         //Transform data
         Transform transform = new Transform();
@@ -33,7 +32,7 @@ public class App {
 //        String format = "csv";
 //        String mode = "overwrite";
 //        String compression = null;
-//        String trg_path = "C:\\Users\\Rahul Kabothula\\Intellij Maven Projects\\Orders\\src\\main\\resources\\result\\retail_db";
+//        String trg_path = "C:\\Users\\Rahul Kabothula\\Intellij Maven Projects\\Orders\\src\\main\\resources\\result";
 //
 //        load.write_data(tdf1, format,mode,compression,trg_path);
     }
--- src/main/java/Extract.java
+++ src/main/java/Extract.java
@@ -2,7 +2,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Row;
 
-public class Read {
+public class Extract {
     public Dataset<Row> readData(SparkSession sparkSession,String src_file_format, String src_file_path){
         return sparkSession
                 .read()
